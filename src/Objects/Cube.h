4b2fd71987e551191afea1be146404289399c12b
Russell Jahn <russelljahn@gmail.com>
2014-02-27 19:28:17 -0600

    Refactored directory structure and added an acid floor to the game.

--- src/Objects/Cube.h
+++
@@ -1,9 +0,0 @@
-#include "GameObject.h"
-
-class Cube : public GameObject {
-	public:
-
-		Cube(Game *attachedGame);
-		void Start();
-
-};
--- src/Player.cpp
+++
@@ -1,9 +0,0 @@
-#include "Player.h"
-
-Player::Player() {
-	mScore = 0;
-}
-void Player::scored(int points) {
-	mScore += points;
-	std::cout << "Player score: " << mScore << std::endl;
-}
